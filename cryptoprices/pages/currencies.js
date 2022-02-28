092b1a819be73013d5d6b34de7a979e988e36940
Tmarra122333 <Tmarra122333@gmail.com>
2022-02-28 10:46:59 -0500

    2nd commit for crypto lab

---
+++ cryptoprices/pages/currencies.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Currencies = (props) => {
+  return <h1>This is the Currencies Component</h1>;
+};
+
+export default Currencies;
---
+++ cryptoprices/pages/price.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Price = (props) => {
+  return <h1>This is the Price Component</h1>;
+};
+
+export default Price;
